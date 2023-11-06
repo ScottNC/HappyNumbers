8f1e7bf473a9f185e3aa150c19a09ecdf0f52337
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-11-06 20:37:32 +0000

    determines if numbers are happy in any base

--- src/main/HappyNumber.java
+++ src/main/HappyNumber.java
@@ -1,9 +1,27 @@
-public class HappyNumber {
-//    public static boolean isHappy (int i, int base) {
-//
-//    }
+import java.util.ArrayList;
+import java.util.List;
 
-    public static int sumDigits (int i, int base) {
+public class HappyNumber {
+    public static boolean isHappy (int i, int base) {
+        List<Integer> allSums = new ArrayList<>();
+
+        int newNumber = i;
+
+        while (true) {
+            Integer sum = sumSquareDigits(newNumber, base);
+
+            if (sum == 1)
+                return true;
+            if (allSums.contains(sum))
+                return false;
+
+            allSums.add(sum);
+
+            newNumber = sum;
+        }
+    }
+
+    public static Integer sumSquareDigits (int i, int base) {
         int upperPower = (int) Math.floor(Math.log(i)/Math.log(base));
 
         int sum = 0;
@@ -12,7 +30,7 @@ public class HappyNumber {
             int powerOfBase = (int) Math.pow(base, power);
             int digitValue = (int) Math.floor((double) i /powerOfBase);
             i -= digitValue * powerOfBase;
-            sum += digitValue;
+            sum += (int) Math.pow(digitValue, 2);
         }
 
         return sum;
--- src/test/HappyNumberTest.java
+++ src/test/HappyNumberTest.java
@@ -3,9 +3,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class HappyNumberTest {
 
     @org.junit.jupiter.api.Test
-    void sumDigits() {
-        assertEquals(10, HappyNumber.sumDigits(1234, 10));
-        assertEquals(25, HappyNumber.sumDigits(55555, 10));
-        assertEquals(2, HappyNumber.sumDigits(20, 2));
+    void sumSquareDigits() {
+        assertEquals(30, HappyNumber.sumSquareDigits(1234, 10));
+        assertEquals(125, HappyNumber.sumSquareDigits(55555, 10));
+        assertEquals(2, HappyNumber.sumSquareDigits(20, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void is10Happy() {
+        assertTrue(HappyNumber.isHappy(1, 10));
+        assertTrue(HappyNumber.isHappy(673, 10));
+        assertTrue(HappyNumber.isHappy(989, 10));
+        assertFalse(HappyNumber.isHappy(2, 10));
+        assertFalse(HappyNumber.isHappy(339, 10));
+    }
+
+    @org.junit.jupiter.api.Test
+    void is6Happy() {
+        assertTrue(HappyNumber.isHappy(254, 6));
+        assertFalse(HappyNumber.isHappy(851, 6));
     }
 }
